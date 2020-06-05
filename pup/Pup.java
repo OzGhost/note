516c02acb35b498711ecfa4a4c9a51b9c560c677
[Vision] Duc <nhduc@axonactive.com>
2020-06-05 10:05:01 +0700

    pup improve

--- pup/Pup.java
+++ pup/Pup.java
@@ -8,15 +8,24 @@ import java.util.concurrent.TimeUnit;
 
 public class Pup {
 
-    private static final LinkedBlockingQueue<Object> q;
-
-    static {
-        q = new LinkedBlockingQueue<>();
-        new Thread(new Writer(q)).start();
-    }
+    private static final LinkedBlockingQueue<Object> q = new LinkedBlockingQueue<>();
+    private static final String ofp = System.getProperty("java.io.tmpdir") + File.separator + "inner_pup.log";
+    private static volatile Thread t;
 
     public static void bark(Object o) {
+    	if (t == null) {
+    		synchronized(Pup.class) {
+    			if (t == null) {
+    				t = new Thread(new Writer(q));
+    				t.start();
+    			}
+    		}
+    	}
         try { q.offer(o, 30, TimeUnit.SECONDS); } catch(Throwable ignored){}
+    }
+    
+    public static void bark(Object o, boolean b) {
+    	try { write(o); } catch(Exception ingored) {}
     }
 
     public static void bark() {
@@ -27,20 +36,29 @@ public class Pup {
         }
     }
 
+    private static void write(Object x) throws Exception {
+        try (FileOutputStream fos = new FileOutputStream(ofp, true); PrintStream ps = new PrintStream(fos, true)) {
+            if (x instanceof Throwable) {
+                ((Throwable) x).printStackTrace(ps);
+            } else {
+                String sx = ""+x;
+                ps.println(sx);
+            }
+        }
+    }
+    
     private static class Writer implements Runnable {
         private final LinkedBlockingQueue<Object> q;
-        private final String ofp;
 
         Writer(LinkedBlockingQueue<Object> oq) {
             q = oq;
-            ofp = System.getProperty("java.io.tmpdir") + File.separator + "inner_pup.log";
         }
 
         @Override
         public void run() {
             Thread me = Thread.currentThread();
-            try { write("=.= Writer installation completed! =.="); } catch(Exception e) { e.printStackTrace(); } 
-            while (!me.isInterrupted()) {
+            try { write("=.= Writer installation completed! =.="); } catch(Exception e) { e.printStackTrace(); }
+            while (!me.isInterrupted() || !q.isEmpty()) {
                 try {
                     Object x = q.poll(30, TimeUnit.SECONDS);
                     if (x != null) write(x);
@@ -49,16 +67,6 @@ public class Pup {
                 }
             }
         }
-
-        private void write(Object x) throws Exception {
-            try (FileOutputStream fos = new FileOutputStream(ofp, true); PrintStream ps = new PrintStream(fos, true)) {
-                if (x instanceof Throwable) {
-                    ((Throwable) x).printStackTrace(ps);
-                } else {
-                    String sx = ""+x;
-                    ps.println(sx);
-                }
-            }
-        }
     }
 }
+
