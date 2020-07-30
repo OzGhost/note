00b0841d4213ee45c61c393d14de4bb5a8b8b0bc
[Vision] Duc <nhduc@axonactive.com>
2020-07-30 08:51:32 +0700

    exam1st counter v2

--- linktink/exam_1st.java
+++ linktink/exam_1st.java
@@ -1,6 +1,13 @@
 
 public class Exam1st {
 
+	private static final int PRESSOR = 2;
+	private static final int LIFTER = ~PRESSOR;
+	private static final byte[] TEMPLATE = "(y,x)".getBytes();
+	private static final byte[] FAILMSG = "NOT FOUND!".getBytes();
+
+	private java.io.OutputStream os;
+
 	@org.junit.Test
 	public void test_me() {
 		int[][] matrix = new int[][]{
@@ -10,59 +17,65 @@ public class Exam1st {
 				new int[]{0,0,0,0}
 		};
 		int w = 4, h = 4;
+		os = System.out;
 		doSearch(matrix, w, h, 0, 1, 3, 3);
 	}
 
 	private void doSearch(int[][] m, int w, int h, int ex, int ey, int ox, int oy) {
 		int l = 0;
 		int[] xb = new int[w*h], yb = new int[w*h];
-		boolean[][] footprint = new boolean[h][w];
-		boolean found = takeAStep(ex, ey, m, w, h, xb, yb, l, ox, oy, footprint);
+		boolean found = takeAStep(ex, ey, m, w, h, xb, yb, l, ox, oy);
 		if (!found) {
-			System.out.println("NOT FOUND!");
+			silentWrite(FAILMSG);
 		}
 	}
 
-	private boolean takeAStep(int cx, int cy, int[][] m, int w, int h, int[] xb, int[] yb, int l, int dx, int dy, boolean[][] footprint) {
+	private void silentWrite(byte... b) {
+		try { os.write(b); } catch(Throwable ignored) {}
+	}
+
+	private boolean takeAStep(int cx, int cy, int[][] m, int w, int h, int[] xb, int[] yb, int l, int dx, int dy) {
 		xb[l] = cx;
 		yb[l] = cy;
 		l++;
-		footprint[cy][cx] = true;
+		m[cy][cx] |= PRESSOR;
 		if (cx == dx && cy == dy) {
 			print(xb, yb, l);
 			return true;
 		}
 		boolean hit = false;
 		int nx = cx+1, ny = cy;
-		if (stepable(nx, ny, m, w, h, footprint)) {
-			hit = takeAStep(nx, ny, m, w, h, xb, yb, l, dx, dy, footprint);
+		if (stepable(nx, ny, m, w, h)) {
+			hit = takeAStep(nx, ny, m, w, h, xb, yb, l, dx, dy);
 		}
 		nx = cx-1; ny = cy;
-		if (!hit && stepable(nx, ny, m, w, h, footprint)) {
-			hit = takeAStep(nx, ny, m, w, h, xb, yb, l, dx, dy, footprint);
+		if (!hit && stepable(nx, ny, m, w, h)) {
+			hit = takeAStep(nx, ny, m, w, h, xb, yb, l, dx, dy);
 		}
 		nx = cx; ny = cy+1;
-		if (!hit && stepable(nx, ny, m, w, h, footprint)) {
-			hit = takeAStep(nx, ny, m, w, h, xb, yb, l, dx, dy, footprint);
+		if (!hit && stepable(nx, ny, m, w, h)) {
+			hit = takeAStep(nx, ny, m, w, h, xb, yb, l, dx, dy);
 		}
 		nx = cx; ny = cy-1;
-		if (!hit && stepable(nx, ny, m, w, h, footprint)) {
-			hit = takeAStep(nx, ny, m, w, h, xb, yb, l, dx, dy, footprint);
+		if (!hit && stepable(nx, ny, m, w, h)) {
+			hit = takeAStep(nx, ny, m, w, h, xb, yb, l, dx, dy);
 		}
 		l--;
-		footprint[cy][cx] = false;
+		m[cy][cx] &= LIFTER;
 		return hit;
 	}
 
 	private void print(int[] xb, int[] yb, int l) {
 		for (int i = 0; i < l; i++) {
-			System.out.print("("+yb[i]+","+xb[i]+")");
+			TEMPLATE[1] = (byte)(yb[i] + '0');
+			TEMPLATE[3] = (byte)(xb[i] + '0');
+			silentWrite(TEMPLATE);
 		}
-		System.out.println();
+		silentWrite((byte)'\n');
 	}
 
-	private boolean stepable(int nx, int ny, int[][] m, int w, int h, boolean[][] footprint) {
-		return nx >=0 && nx < w && ny >=0 && ny < h && m[ny][nx] == 0 && !footprint[ny][nx];
+	private boolean stepable(int nx, int ny, int[][] m, int w, int h) {
+		return nx >=0 && nx < w && ny >=0 && ny < h && m[ny][nx] == 0;
 	}
 }
 
