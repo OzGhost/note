a22c0d93ecd50f33643762e4d73d2e655789f2fa
[Vision] Duc <nhduc@axonactive.com>
2020-08-13 13:37:42 +0700

    init project

---
+++ linktink/qpro/src/main/java/org/qpro/YourAnswer.java
@@ -0,0 +1,12 @@
+package org.qpro;
+
+import java.io.*;
+
+public class YourAnswer {
+
+    public void answerTo(InputStream input, OutputStream output) throws Exception {
+        // insert your answer please 
+    }
+
+}
+
