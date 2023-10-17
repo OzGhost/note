c0626e440040607498514b7f6ab19ce02d03e12b
Oz <xad.love@gmail.com>
2023-10-17 22:02:14 +0700

    kbsim | ditch promise

--- kbsim-ext/ground.js
+++ kbsim-ext/ground.js
@@ -1,18 +1,18 @@
-console.log("__[o0] i'm in");
-var inused = false;
-var cancelled = false;
-function unwire(){ inused = false; }
+var ctx = {};
+var verbose = false;
+var cid = 0;
+verbose && console.log("__[o0] i'm in");
 
 function runControl(payload) {
-    if (inused)
-        return;
+    if (ctx.cmds)
+        return verbose && console.warn("__[xx] busy");
     if (payload == "ibks") {
         browser.storage.sync.get("cmds").then(runControl);
         return;
     }
     var cmds = payload.cmds;
     if (!cmds) {
-        console.warn("no cfg found > load default cfg ...");
+        verbose && console.warn(" .. load default commands!");
         cmds = " ul ul ul ul ul ul ul ul ul ul ul ul"
                 + " rd rd rd rd rd rd rd rd rd rd rd rd rd"
                 + "rrru"
@@ -26,24 +26,42 @@ function runControl(payload) {
                 + " rd rd rd rd rd rd rd rd rd rd rd rd rd"
         ;
     }
-    inused = true;
-    cancelled = false;
-    console.log("1 sec ...");
-    setTimeout(function(){
-        var anc = Promise.resolve();
-        for (var i = 0; i < cmds.length; i++) {
-            var d = cmds.charAt(i);
-            if (d == " " || d == "\n")
-                continue;
-            anc = digest(d, cmds, i, anc);
-        }
-        anc.then(unwire, unwire);
-    }, 1000);
+    ctx = { cmds: cmds, idx: 0 };
+    verbose && console.log("1 sec ...");
+    cid = setTimeout(roll, 500);
 }
-function digest(c, cmds, i, anc) {
-    var synev = undefined;
-    var waitSig = false;
-    switch (c) {
+
+function roll() {
+    if (ctx.fn) {
+        var lfn = ctx.fn;
+        ctx.fn = 0;
+        return (cid = setTimeout(roll, lfn()));
+    }
+    if (!ctx.cmds) {
+        return verbose && console.log(" .. cancelled!");
+    }
+    var p = 0;
+    for (var i = ctx.idx; i < ctx.cmds.length; i++) {
+        p = digest(ctx.cmds.charAt(i), i);
+        if (p)
+            break;
+    }
+    if (!p)
+        return (ctx = {});
+    ctx.idx = p.i;
+    p.subFn && (ctx.fn = p.subFn);
+    if (p.fn) {
+        var delay = p.fn();
+        cid = setTimeout(roll, delay);
+        if (delay < 200)
+            cid = 0;
+    }
+}
+
+function digest(d, i) {
+    var synev = 0;
+    var waitSig = 0;
+    switch (d) {
         case "u":
             synev = {code:"ArrowUp",key:"ArrowUp",keyCode:38,which:38,bubbles:true};
             break;
@@ -63,53 +81,57 @@ function digest(c, cmds, i, anc) {
             break;
     }
     if (synev)
-        return anc.then(function(){
-                    return cancelled
-                        ? Promise.reject(new Error("cbks"))
-                        : press(synev);
-                    });
+        return { fn: press(synev), subFn: release(synev), i: i+1 };
     if (waitSig) {
         var ni = i+1;
-        var nc = cmds.charAt(ni);
+        var nc = ctx.cmds.charAt(ni);
         var sec = "";
         var l = 0;
         while ("0" <= nc && nc <= "9" && l < 5) {
             sec += nc;
             ni++;
-            nc = cmds.charAt(ni);
+            nc = ctx.cmds.charAt(ni);
             l++;
         }
         if (sec)
-            return anc.then(function(){
-                    return cancelled
-                        ? Promise.reject(new Error("cbks"))
-                        : rest(1000*sec);
-                    });
+            return { fn: idle(sec), i: ni };
     }
-    return anc;
 }
+
 function press(synev) {
-    return new Promise(function(resolve, reject){
+    return function() {
+        verbose && console.log("_ press: ", synev.key);
         document.body.dispatchEvent(new KeyboardEvent("keydown", synev));
-        setTimeout(function(){
-            document.body.dispatchEvent(new KeyboardEvent("keyup", synev));
-            setTimeout(resolve, pickin(50, 80));
-        }, pickin(60, 100));
-    });
+        return pickin(65, 108);
+    };
 }
+
 function pickin(low, hig) {
     return Math.round(Math.random() * (hig - low)) + low;
 }
-function rest(delay) {
-    return new Promise(function(resolve, reject){
-        setTimeout(resolve, delay + pickin(1, 99));
-    });
+
+function release(synev) {
+    return function() {
+        verbose && console.log("_ release: ", synev.key);
+        document.body.dispatchEvent(new KeyboardEvent("keyup", synev));
+        return pickin(55, 88);
+    };
+}
+
+function idle(t) {
+    return function(){
+        verbose && console.log("_ idle: ", t);
+        return 1000*t + pickin(11, 77);
+    };
 }
 
 function onKeyFn(e) {
-    if (e.key == "Control")
-        cancelled = true;
-    if (e.shiftKey && (e.key == "M" || e.key == "m"))
+    if (e.key == "Control") {
+        verbose && console.log("__ cancelling ...");
+        clearTimeout(cid);
+        ctx.cmds = 0;
+    }
+    if (e.shiftKey && (e.key == "." || e.key == ">"))
         runControl("ibks");
 
 }
