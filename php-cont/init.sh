d9c1b971bba9e02777faacd79571f143bd211da2
lw14134 <lw14134@linecorp.com>
2024-05-01 20:11:34 +0700

    php-cont | draft

---
+++ php-cont/init.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+docker container rm -f mahpo >/dev/null 2>&1
+docker container rm -f mahdbs >/dev/null 2>&1
+docker network rm -f mahnet
+
+docker network create mahnet
+docker container create --name mahdbs \
+	-e MYSQL_ROOT_PASSWORD=Edo123 \
+	-e MYSQL_DATABASE=mahdb \
+	--network mahnet \
+	--network-alias mahdb \
+	mysql:8.4.0
+docker container create --name mahpo \
+	-e MYSQL_ROOT_PASSWORD=nah \
+	-e MYSQL_DATABASE=rock \
+	--network mahnet \
+	-p 3000:80 \
+	mahphp:0.0.1
