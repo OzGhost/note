6966f98b7cbc7452ef4351bebc584ca89d36b216
OzGhost <xad.love@gmail.com>
2024-09-12 18:00:22 +0700

    kbsim | support 2nd addr

--- rpgmo-ana/reload.sh
+++ rpgmo-ana/reload.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/bin/tcsh
 curl -O https://data.mo.ee/release.js
+echo "var npc_base = [];" > mobs.js
+cat release.js | tr -d '\n' | sed -e 's|\(npc_base\[[^(]*createObject(\)|\n\1|g' | grep '^npc' | grep -v 'fn:{' | sed '$ s|;.*|;|' >> mobs.js
 echo "var item_base = [];" > items.js
 cat release.js | tr -d '\n' | sed -e 's|;|;\n|g' | grep '^item_base\[[0-9]\+' >> items.js
-echo "var npc_base = [];" > mobs.js
-cat release.js | tr -d '\n' | sed -e 's|\(npc_base\[[0-9]\+\)|\n\1|g' | grep '^npc_base\[[0-9]*\]=createObject' | sed -e '$s|;.*|;|' -e 's|fn:{.*}},|fn:{}},|' >> mobs.js
 echo "var pets = [];" > pets.js
 cat release.js | tr -d '\n' | sed -e 's|pets\[|\npets\[|g' | grep '^pets\[[0-9]*\]=createObject' | sed -e '$s|;.*|;|' >> pets.js
