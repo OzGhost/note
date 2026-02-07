f1d0317c615cec51aab9136d735e48985b95dbd3
Dyce <xad.love@gmail.com>
2026-02-07 12:02:12 +0700

    netio | fix multi interface read

--- net-io.sh
+++ net-io.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 while true; do
-    sleep 10
-    ip -s link | grep wlp -A 5 | tail -n +4 | grep -v TX | awk '{print $1}' | {
+    ip -s link | grep wlp -A 5 | tail -3 | grep -v TX | awk '{print $1}' | {
         read down
         read up
         echo -n "down: "
@@ -9,4 +8,5 @@ while true; do
         echo -n "  up: "
         echo "print( round( $up / (1024*1024) * 1000 ) / 1000)" | python -
     }
+    sleep 10
 done
