a786603fd68b8dc45c3d4e5a40daa1f77c282dff
Dyce <xad.love@gmail.com>
2026-02-28 08:26:15 +0700

    net-io | add deli + shorten the tail

--- net-io.sh
+++ net-io.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 while true; do
+    echo "---"
     ip -s link | grep wlp -A 5 | tail -3 | grep -v TX | awk '{print $1}' | {
         read down
         read up
         echo -n "down: "
-        echo "print( round( $down / (1024*1024) * 1000 ) / 1000)" | python -
+        echo "print( round( $down / (1024*1024) * 10 ) / 10)" | python -
         echo -n "  up: "
-        echo "print( round( $up / (1024*1024) * 1000 ) / 1000)" | python -
+        echo "print( round( $up / (1024*1024) * 10 ) / 10)" | python -
     }
     sleep 10
 done
