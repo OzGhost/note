200822e5e26ec360441921fd3c9ad487b2f9f10f
Dyce <xad.love@gmail.com>
2026-04-21 06:10:36 +0700

    net-io | drop python

--- net-io.sh
+++ net-io.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 while true; do
-    echo "---"
     ip -s link | grep wlp -A 5 | tail -3 | grep -v TX | awk '{print $1}' | {
         read down
         read up
-        echo -n "down: "
-        echo "print( round( $down / (1024*1024) * 10 ) / 10)" | python -
-        echo -n "  up: "
-        echo "print( round( $up / (1024*1024) * 10 ) / 10)" | python -
+        ld=${#down}
+        lu=${#up}
+        nd=0
+        nu=0
+        if [ $ld -gt 5 ]; then
+            t=$((ld-5))
+            nd=$(echo ${down:0:$t} | sed 's|\(.\)$|.\1|')
+        fi
+        if [ $lu -gt 5 ]; then
+            t=$((lu-5))
+            nu=$(echo ${up:0:$t} | sed 's|\(.\)$|.\1|')
+        fi
+        printf "\n> down: $nd | up: $nu "
     }
     sleep 10
 done
